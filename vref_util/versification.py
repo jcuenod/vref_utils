d4dda54916b7bc45be68699522b095ea5fc9d8b6
jcuenod <4253884+jcuenod@users.noreply.github.com>
2024-08-08 17:16:19 -0500

    Add documentation + support custom vref versification file

--- vref_util/versification.py
+++ vref_util/versification.py
@@ -3,8 +3,13 @@ import os
 # distributed with ./vref.txt - the "org" (standard) versification vref file
 org_vref_dot_txt_path = os.path.join(os.path.dirname(__file__), "vref.txt")
 
-def get_versification_mapping(vrs_path=org_vref_dot_txt_path):
-    with open(vrs_path) as f:
+def get_versification_mapping(versification_vref_path=org_vref_dot_txt_path):
+    """
+    Returns a mapping of verse references to line numbers based on a vref
+    file that has a reference on each line corresponding to verse-per-line
+    vref files that use the same versification system.
+    """
+    with open(versification_vref_path) as f:
         vref = f.readlines()
         vref = {
             line.strip(): index + 1
@@ -13,6 +18,10 @@ def get_versification_mapping(vrs_path=org_vref_dot_txt_path):
     return vref
 
 def get_versification_range(start, end, versification_mapping):
+    """
+    Returns a list of verse references between the start and end verse
+    references
+    """
     reverse_mapping = {
         value: key
         for key, value in versification_mapping.items()
--- vref_util/vref.py
+++ vref_util/vref.py
@@ -1,20 +1,35 @@
 import linecache
 from .verse import Verse
 from .versification import get_versification_mapping, get_versification_range
+from typing import List
 
 class Vref:
-    def __init__(self, vref_file):
+    def __init__(self, vref_file, versification_vref_path=None):
         self.vref_file = vref_file
-        self.verse_to_line_mappings = get_versification_mapping()
+        self.verse_to_line_mappings = get_versification_mapping() if versification_vref_path is None else get_versification_mapping(versification_vref_path)
 
-    # Supports ["GEN 1:1"], ["GEN 1:1,GEN 1:2-GEN 1:3"], ["GEN 1:1-3"] etc.
-    # Requires , for multiple selections and - for ranges.
-    # Requires USFM book names.
-    # No implicit books or chapters. 
-    def __getitem__(self, key: str):
+    def __getitem__(self, key: str) -> List[Verse]:
+        """
+        Returns a list of Verse objects based on the key provided. The key
+        can be a single verse reference, a comma-separated list of verse
+        references, or a range of verse references. The verse references
+        can be in the format "GEN 1:1" or "GEN 1:1-3". The verse references
+        must be in the USFM book name format and books and chapters must be
+        explicitly stated (no implicit books or chapters).
+
+        Supported formats:
+        - "GEN 1:1"
+        - "GEN 1:1,GEN 1:2-GEN 1:3"
+        - "GEN 1:1-3"
+
+        Args:
+            key (str): The verse reference or references to retrieve.
+
+        Returns:
+            list[Verse]: A list of Verse objects for the requested verses.
+        """
         if "," in key or "-" in key:
             return self._get_ranges_and_selections(key)
-        
         return [self._get_verse(key)]
     
     def _get_ranges_and_selections(self, key):
