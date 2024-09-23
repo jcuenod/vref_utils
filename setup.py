6564008fd885eaedc3179caed54b641b85a65a6f
jcuenod <4253884+jcuenod@users.noreply.github.com>
2024-09-23 10:18:57 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="vref_utils",
-    version="0.0.5",
+    version="0.0.6",
     packages=find_packages(),
     author="James Cuénod",
     author_email="j3frea+github@gmail.com",
